cc5bd61e2873cdfd4e27d75dff30af3203a9807e
70sh1 <70sh1@proton.me>
2023-11-11 17:47:44 +0300

    Fix version

--- eddy.go
+++ eddy.go
@@ -21,7 +21,7 @@ func main() {
 	app := &cli.App{
 		Name:                   "eddy",
 		Usage:                  "simple and fast file encryption",
-		Version:                "1.0.9",
+		Version:                "1.0.0",
 		UseShortOptionHandling: true,
 		Suggest:                true,
 		Flags: []cli.Flag{
