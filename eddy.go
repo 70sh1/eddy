d2bd9f975bd61a76b8002be83c45fae5ac430843
70sh1 <70sh1@proton.me>
2023-12-24 13:49:19 +0300

    v1.2.2 prep

--- eddy.go
+++ eddy.go
@@ -17,7 +17,7 @@ func main() {
 	app := &cli.App{
 		Name:                   "eddy",
 		Usage:                  "simple and fast file encryption",
-		Version:                "1.2.1",
+		Version:                "1.2.2",
 		UseShortOptionHandling: true,
 		Suggest:                true,
 		Before: func(ctx *cli.Context) error {
