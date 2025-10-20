5949d0cf477cb3d00bb81932f7e86c8f8687bcf1
70sh1 <70sh1@proton.me>
2025-10-20 05:49:59 +0300

    build: bump version

--- eddy.go
+++ eddy.go
@@ -24,7 +24,7 @@ func main() {
 	app := &cli.App{
 		Name:    "eddy",
 		Usage:   "simple and fast file encryption",
-		Version: "1.5.1",
+		Version: "1.5.2",
 		Authors: []*cli.Author{
 			{Name: "70sh1", Email: "70sh1@proton.me"},
 		},
